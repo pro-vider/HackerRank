c66e8fed716e9dc779e6cc03ab74df8e947f3e2d
kannu priya <imkannupriya5@gmail.com>
2021-10-30 14:09:46 +0530

    Common Child

---
+++ Common Child.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the commonChild function below.
+    static int commonChild(String s1, String s2) {
+    int[][] C = new int[s1.length()+1][s2.length()+1];
+
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    C[i+1][j+1] = C[i][j] + 1;
+                } else {
+                    C[i+1][j+1] = Math.max(C[i+1][j], C[i][j+1]);
+                }
+            }
+        }
+        for (int i = 0; i < s1.length(); i++) {
+        }
+
+        return C[s1.length()][s2.length()];
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new                           FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = scanner.nextLine();
+
+        String s2 = scanner.nextLine();
+
+        int result = commonChild(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
